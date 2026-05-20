8f4a628dc0c6f5580a984559bc27acdfa3618fdf
uditmahajann <uditmahajan95@gmail.com>
2026-05-20 16:16:13 +0530

    Sitemap added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,40 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://www.mcspathankot.edu.in',
+
+  integrations: [
+    react(),
+    sitemap()
+  ],
 
   vite: {
     plugins: [tailwindcss()]
   }
 });
+
+
+
+
+
+
+
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+// import react from '@astrojs/react';
+// import tailwindcss from '@tailwindcss/vite';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [react()],
+
+//   vite: {
+//     plugins: [tailwindcss()]
+//   }
+// });
 
 
